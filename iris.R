f48655d1d1da8f57fd75b2169b2a548a79f978d9
laura.noguer <laura.perez.n@gmail.com>
2024-02-07 11:46:57 +0100

    Added a plot step

--- iris.R
+++ iris.R
@@ -7,3 +7,8 @@ iris_setosa <- iris |>
 
 iris_versicolor <- iris |>
   filter(Species == "versicolor")
+library(ggplot2)
+
+ggplot(iris_setosa, aes(x = Petal.Length, y = Sepal.Length)) +
+  geom_point() +
+  geom_smooth(method = "lm")
